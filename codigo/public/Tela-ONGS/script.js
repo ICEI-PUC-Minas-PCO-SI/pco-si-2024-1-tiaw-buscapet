8a9e4995e14009c1b885a690c6119b82d124ef76
breno_pinheir0 <breno.oliveira@hotmail.com.br>
2024-06-22 15:53:01 -0300

    Breno - Alteração na página de ONGs para colocar o formulário

--- codigo/public/Tela-ONGS/script.js
+++ codigo/public/Tela-ONGS/script.js
@@ -36,7 +36,6 @@ window.onload = () => {
     .then(data => {
              
         escrever()
-        salvaDados(data)
         db = data;
         imprimeOngs()
     })
@@ -76,11 +75,6 @@ const pesquisar = () => {
 
         naoEncontrado.style.display = 'none'
     }
-}
-
-function salvaDados(dados){
-
-   
 }
 
 function imprimeOngs() {
@@ -140,3 +134,143 @@ function imprimeOngs() {
 
     }
 }
+
+var form1 = document.getElementById("form1");
+var form2 = document.getElementById("form2");
+
+var Proximo = document.getElementById("Proximo");
+var Voltar = document.getElementById("Voltar");
+
+Proximo.onclick = function() {
+  
+  form1.style.left = "-850px";
+  form2.style.left = "80px";
+  progress.style.width = "760px";
+
+}
+
+Voltar.onclick = function() {
+  
+    form1.style.left = "80px";
+    form2.style.left = "-850px";
+    progress.style.width = "370px";
+}
+
+
+function leDados() {
+
+  let strDados = localStorage.getItem('db');
+  let objDados = {};
+
+  if (strDados) {
+
+    objDados = JSON.parse(strDados);
+
+  } else {
+
+    objDados = {contatos: []}
+  }
+
+  return objDados;
+}
+
+function salvaDados(dados) {
+
+  localStorage.setItem('db', JSON.stringify(dados));
+}
+
+function incluirContato() {
+  
+  let objDados = leDados();
+
+  let strNome = document.getElementById('nome').value
+  let strTelefone = document.getElementById('telefone').value
+  let strWhatsapp = document.getElementById('whatsapp').value
+  let strEndereco = document.getElementById('endereco').value
+  let strEmail = document.getElementById('email').value
+  let strEstado = document.getElementById('estado').value
+  let strFrequenciaAdocao = document.getElementById('frequenciaAdocao').value
+  let strQuantosAnimaisProtecao = document.getElementById('quantosAnimaisProtecao').value
+  let strTempoFilaDeAdocaoo = document.getElementById('tempoFilaDeAdocao').value
+  let strTempoCuidadoso = document.getElementById('tempoCuidados').value
+  let strRecolhemAnimais = document.querySelector('#recolhemAnimais').classList.contains('active') ? 'Sim' : 'Não';
+  let strAceitamDoenteso = document.querySelector('#aceitamDoentes').classList.contains('active') ? 'Sim' : 'Não';
+
+  let novoContato = {
+
+    nome: strNome,
+    telefone: strTelefone,
+    whatsapp: strWhatsapp,
+    endereco: strEndereco,
+    email: strEmail,
+    estado: strEstado,
+    frequenciaAdocao: strFrequenciaAdocao,
+    tempoCuidados: strTempoCuidadoso,
+    tempoFilaDeAdocao: strTempoFilaDeAdocaoo,
+    quantosAnimaisProtecao: strQuantosAnimaisProtecao,
+    recolhemAnimais: strRecolhemAnimais,
+    aceitamDoentes: strAceitamDoenteso
+  };
+
+  if (strFrequenciaAdocao > 1) {
+    novoContato.frequenciaAdocao = "Muito";
+  } else if (strFrequenciaAdocao == 1) {
+    novoContato.frequenciaAdocao = "Médio";
+  } else {
+    novoContato.frequenciaAdocao = "Pouco";
+  }
+
+  if (strTempoCuidadoso > 1) {
+    novoContato.tempoCuidados = "Muito";
+  } else if (strTempoCuidadoso == 1) {
+    novoContato.tempoCuidados = "Médio";
+  } else {
+    novoContato.tempoCuidados = "Pouco";
+  }
+
+  if (strTempoFilaDeAdocaoo > 1) {
+    novoContato.tempoFilaDeAdocao = "Muito";
+  } else if (strTempoFilaDeAdocaoo == 1) {
+    novoContato.tempoFilaDeAdocao = "Médio";
+  } else {
+    novoContato.tempoFilaDeAdocao = "Pouco";
+  }
+
+  if (strQuantosAnimaisProtecao > 1) {
+    novoContato.quantosAnimaisProtecao = "Muito";
+  } else if (strQuantosAnimaisProtecao == 1) {
+    novoContato.quantosAnimaisProtecao = "Médio";
+  } else {
+    novoContato.quantosAnimaisProtecao = "Pouco";
+  }
+
+  objDados.contatos.push(novoContato);
+
+  salvaDados(objDados);
+  alert("Formulário enviado com sucesso!");
+  imprimeDados();
+}
+
+function imprimeDados() {
+
+  let tela = document.getElementById('tela');
+  let strHTML = '';
+  let objDados = leDados();
+
+  for (i = 0; i < objDados.contatos.length; i++) {
+    strHTML += `<p>${objDados.contatos[i].nome} - ${objDados.contatos[i].telefone} - ${objDados.contatos[i].whatsapp} - ${objDados.contatos[i].endereco} - ${objDados.contatos[i].email} - ${objDados.contatos[i].estado} - ${objDados.contatos[i].frequenciaAdocao} - ${objDados.contatos[i].tempoCuidados} - ${objDados.contatos[i].tempoFilaDeAdocao} - ${objDados.contatos[i].quantosAnimaisProtecao} - ${objDados.contatos[i].recolhemAnimais} - ${objDados.contatos[i].aceitamDoentes} </p>`
+  }
+
+  tela.innerHTML = strHTML;
+}
+
+document.getElementById('btnIncluirContato').addEventListener('click', incluirContato);
+
+document.addEventListener('DOMContentLoaded', function () {
+  const estadoDropdown = document.getElementById('estado');
+  estadoDropdown.addEventListener('change', function (event) {
+    const selectedEstado = event.target.value;
+    const formState = {selectedEstado};
+    localStorage.setItem('formState', JSON.stringify(formState));
+  });
+});
