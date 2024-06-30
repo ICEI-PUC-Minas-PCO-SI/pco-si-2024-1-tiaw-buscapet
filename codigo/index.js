ec74f3e7ccbc43bca797b67f285ae09d8c0c0c91
IngridYara <Yaraalves3632659@gmail.com>
2024-06-30 01:13:10 -0300

    Ingrid - criando os arquivos para o vercel

--- codigo/index.js
+++ codigo/index.js
@@ -7,7 +7,14 @@ const middlewares = jsonServer.defaults()
 
 server.use(cors())
 server.use(middlewares)
+server.use(
+  jsonServer.rewriter({
+    "/*": "/$1",
+  })
+)
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server está em execução em http://localhost:3000!')
 })
+
+module.exports = server;
